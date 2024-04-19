1f6d2183fafcb3138c83f66d7ae38e2e9393a416
efraimribeirooliveira <159633219+efraimribeirooliveira@users.noreply.github.com>
2024-04-18 22:44:26 -0300

    Finalizei o site.

--- modelos/script.js
+++ modelos/script.js
@@ -1,8 +1,12 @@
+var menu = document.querySelector('#teste')
+var menuCover = document.querySelector('.menu-cover');
+
 function abrirMenu() {
-    var menu = document.querySelector('#teste')
     if (menu.style.display === 'none') {
         menu.style.display = 'block'
+        menuCover.style.display = 'block'
     } else {
         menu.style.display = 'none'
+        menuCover.style.display = 'none'
     }
 }
